160e32d528332ea9215ee0293e8f90dddebb777b
Sohaib <sohaibahmed.hypersoft@gmail.com>
2022-12-26 16:46:29 +0500

    In-App Update

--- app/src/main/java/dev/epegasus/inappupdate/MainActivity.kt
+++ app/src/main/java/dev/epegasus/inappupdate/MainActivity.kt
@@ -1,29 +1,17 @@
 package dev.epegasus.inappupdate
 
-import android.content.IntentSender.SendIntentException
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.play.core.appupdate.AppUpdateInfo
-import com.google.android.play.core.appupdate.AppUpdateManager
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.common.IntentSenderForResultStarter
 import com.google.android.play.core.install.model.AppUpdateType
-import com.google.android.play.core.install.model.InstallStatus
-import com.google.android.play.core.install.model.UpdateAvailability
 import dev.epegasus.inappupdate.databinding.ActivityMainBinding
-
-private const val MY_REQUEST_CODE = 1000
-private const val TAG = "MyTag"
+import dev.epegasus.inappupdate.manager.PegasusAppUpdateManager
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private val appUpdateManager by lazy { AppUpdateManagerFactory.create(this) }
+    private val pegasusAppUpdateManager = PegasusAppUpdateManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,82 +20,40 @@ class MainActivity : AppCompatActivity() {
         checkForUpdate()
     }
 
-    private fun checkForUpdate() {
-
-        // Returns an intent object that you use to check for an update.
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-
-        // Checks that the platform will allow the specified type of update.
-        // This example applies an immediate update. To apply a flexible update instead, pass in AppUpdateType.FLEXIBLE
-        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-            when (appUpdateInfo.updateAvailability()) {
-                UpdateAvailability.UNKNOWN -> showToast("Unknown Response")
-                UpdateAvailability.UPDATE_NOT_AVAILABLE -> showToast("No Update Available")
-                UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS -> showToast("Update is in Progress. Please Resume")
-                UpdateAvailability.UPDATE_AVAILABLE -> {
-                    showToast("Update Available")
-                    if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) requestForUpdate(appUpdateManager, appUpdateInfo)
-                }
-            }
-        }.addOnFailureListener {
-            showToast("Failed to fetch Data")
-        }
-    }
-
     /**
-     * @see : MY_REQUEST_CODE -> Ignore this Constant
+     *    Types of AppUpdate:
+     *     -> AppUpdateType.IMMEDIATE
+     *     -> AppUpdateType.FLEXIBLE
      */
 
-    private fun requestForUpdate(appUpdateManager: AppUpdateManager, appUpdateInfo: AppUpdateInfo) {
-        // Pass the intent that is returned by 'getAppUpdateInfo()'.
-        // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
-        // The current activity making the update request.
-        // Include a request code to later monitor this update request.
-        val starter = IntentSenderForResultStarter { intent, _, fillInIntent, flagsMask, flagsValues, _, _ ->
-            val request = IntentSenderRequest.Builder(intent).setFillInIntent(fillInIntent).setFlags(flagsValues, flagsMask).build()
-            updateFlowResultLauncher.launch(request)
-        }
-        try {
-            appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, starter, MY_REQUEST_CODE)
-        } catch (ex: SendIntentException) {
-            showToast("Exception Found")
-            Log.d(TAG, "requestForUpdate: $ex")
+    private fun checkForUpdate() {
+        pegasusAppUpdateManager.setUpdateType(AppUpdateType.IMMEDIATE)
+        pegasusAppUpdateManager.checkForUpdate { isAvailable, message ->
+            showToast(message)
+            if (isAvailable) {
+                requestForUpdate()
+            }
         }
     }
 
-    private val updateFlowResultLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-            showToast("Updated Successfully")
-            checkIfUpdateInstalled()
-        } else {
-            showToast("Update Provoked")
+    private fun requestForUpdate() {
+        pegasusAppUpdateManager.requestForUpdate { isUpdated, message ->
+            showToast(message)
+            if (isUpdated) {
+                // Proceed with Code...
+                Log.d("TAG", "requestForUpdate: Running App...")
+            }
         }
     }
+
+    // Checks that the update is not stalled during 'onResume()'
+    override fun onResume() {
+        super.onResume()
+        pegasusAppUpdateManager.checkIfUpdateInstalled()
+    }
+
 
     private fun showToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
-    // Checks that the update is not stalled during 'onResume()'.
-    // However, you should execute this check at all app entry points.
-    override fun onResume() {
-        super.onResume()
-        checkIfUpdateInstalled()
-    }
 
-    private fun checkIfUpdateInstalled() {
-        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
-            // If the update is downloaded but not installed,
-            // notify the user to complete the update.
-            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
-                popupSnackbarForCompleteUpdate()
-            }
-        }
-    }
-
-    // Displays the snackbar notification and call to action.
-    private fun popupSnackbarForCompleteUpdate() {
-        Snackbar.make(binding.root, "An update has just been downloaded.", Snackbar.LENGTH_INDEFINITE).apply {
-            setAction("RESTART") { appUpdateManager.completeUpdate() }
-            show()
-        }
-    }
 }
---
+++ app/src/main/java/dev/epegasus/inappupdate/manager/PegasusAppUpdateManager.kt
@@ -0,0 +1,124 @@
+package dev.epegasus.inappupdate.manager
+
+import android.content.IntentSender
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.common.IntentSenderForResultStarter
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
+import dev.epegasus.inappupdate.MainActivity
+import java.lang.ref.WeakReference
+
+/**
+ * @Author: SOHAIB AHMED
+ * @Date: 26,December,2022
+ * @Accounts
+ *      -> https://github.com/epegasus
+ *      -> https://stackoverflow.com/users/20440272/sohaib-ahmed
+ */
+
+class PegasusAppUpdateManager(private val activity: MainActivity) {
+
+    private val appUpdateManager by lazy { AppUpdateManagerFactory.create(activity) }
+    private val weakReference = WeakReference(activity)
+
+    private var updateType = AppUpdateType.IMMEDIATE
+
+    private var appUpdateInfo: AppUpdateInfo? = null
+    private var callback: ((isUpdated: Boolean, message: String) -> Unit)? = null
+
+    /**
+     *    Types of AppUpdate:
+     *     -> AppUpdateType.IMMEDIATE (1)
+     *     -> AppUpdateType.FLEXIBLE (0)
+     */
+
+    fun setUpdateType(updateType: Int = AppUpdateType.IMMEDIATE) {
+        this.updateType = updateType
+    }
+
+    /**
+     *    Types of UpdateAvailability:
+     *     -> UpdateAvailability.UNKNOWN
+     *     -> UpdateAvailability.UPDATE_NOT_AVAILABLE
+     *     -> UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+     *     -> UpdateAvailability.UPDATE_AVAILABLE
+     */
+
+    fun checkForUpdate(callback: (isAvailable: Boolean, message: String) -> Unit) {
+        appUpdateManager.appUpdateInfo.addOnCompleteListener {
+            if (it.isSuccessful) {
+                appUpdateInfo = it.result
+                when (appUpdateInfo?.updateAvailability()) {
+                    UpdateAvailability.UNKNOWN -> callback.invoke(false, "Unknown Response")
+                    UpdateAvailability.UPDATE_NOT_AVAILABLE -> callback.invoke(false, "No Update Available")
+                    UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS -> callback.invoke(false, "Update is in Progress. Please Resume")
+                    UpdateAvailability.UPDATE_AVAILABLE -> {
+                        if (appUpdateInfo?.isUpdateTypeAllowed(updateType) == true) callback.invoke(true, "Update Available")
+                        else callback.invoke(false, "This type of update is not available.")
+                    }
+                }
+            } else {
+                callback.invoke(false, it.exception?.message.toString())
+            }
+        }
+    }
+
+    /**
+     * @see : MY_REQUEST_CODE -> Ignore this Constant
+     */
+
+    fun requestForUpdate(callback: (isUpdated: Boolean, message: String) -> Unit) {
+        this.callback = callback
+
+        val starter = IntentSenderForResultStarter { intent, _, fillInIntent, flagsMask, flagsValues, _, _ ->
+            val request = IntentSenderRequest.Builder(intent).setFillInIntent(fillInIntent).setFlags(flagsValues, flagsMask).build()
+            updateFlowResultLauncher?.launch(request)
+        }
+        try {
+            appUpdateInfo?.let { info ->
+                appUpdateManager.startUpdateFlowForResult(info, AppUpdateType.IMMEDIATE, starter, 100)
+            } ?: kotlin.run {
+                callback.invoke(false, "Failed to Launch Update Flow, try again later")
+            }
+        } catch (ex: IntentSender.SendIntentException) {
+            callback.invoke(false, ex.message.toString())
+        }
+    }
+
+    private val updateFlowResultLauncher = weakReference.get()?.registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            checkIfUpdateInstalled()
+        } else {
+            callback?.invoke(false, "Update Cancelled by User")
+        }
+    }
+
+    /**
+     *  If the update is downloaded but not installed, notify the user to complete the update.
+     */
+
+    fun checkIfUpdateInstalled() {
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            when (appUpdateInfo.installStatus()) {
+                InstallStatus.DOWNLOADED -> popupSnackbarForCompleteUpdate()
+                InstallStatus.INSTALLED -> callback?.invoke(true, "Updated Successfully")
+                InstallStatus.CANCELED -> callback?.invoke(false, "Cancelled by User")
+                else -> callback?.invoke(false, "Failed to Update")
+            }
+        }
+    }
+
+    // Displays the snackbar notification and call to action.
+    private fun popupSnackbarForCompleteUpdate() {
+        Snackbar.make(activity.findViewById(android.R.id.content), "An update has just been downloaded.", Snackbar.LENGTH_INDEFINITE).apply {
+            setAction("RESTART") { appUpdateManager.completeUpdate() }
+            show()
+        }
+    }
+}
